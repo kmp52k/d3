99f6da313763182c11eb752ae16b3e25ea4be19c
Krunal <krunal.purohit01@infosys.com>
2019-10-23 21:58:44 +0530

    court vision depencies.

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ export { bisect } from "d3-array";
 export { interpolateNumber } from "d3-interpolate";
 export { scalePow, scaleBand } from "d3-scale";
 export { select, selectAll, event } from "d3-selection";
+export { Area, CurveFactory } from "d3-shape";
 export { transition } from "d3-transition";
