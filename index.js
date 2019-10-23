9382dc9500a534397d6a1a4bb9400a584df3076e
Krunal <krunal.purohit01@infosys.com>
2019-10-23 22:10:16 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export { bisect } from "d3-array";
+export * from "d3-axis";
 export { interpolateNumber } from "d3-interpolate";
 export { scalePow, scaleBand } from "d3-scale";
 export { select, selectAll, event } from "d3-selection";
-export { Area, CurveFactory } from "d3-shape";
+export * from "d3-shape";
 export { transition } from "d3-transition";
